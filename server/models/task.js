262cffd3df4418296899ed73d5039d6cdd74e101
Sude Önder <sudeonder999@gmail.com>
2024-06-06 17:26:35 +0300

    define task schema

---
+++ server/models/task.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose";
+
+// Create a schema for tasks
+const taskSchema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  details: {
+    type: String,
+    lowercase: true,
+    maxLength: 200,
+  },
+  userId: {
+    type: String,
+    required: true,
+  },
+  tags: [],
+  selectedFile: String,
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+  status: {
+    type: String,
+    required: true,
+    enum: ["in-progress", "completed", "to-do"],
+    default: "to-do",
+  },
+  priortiy: {
+    type: String,
+    required: true,
+    enum: ["low", "medium", "high"],
+    default: "medium",
+  },
+});
+
+// Compile model from schema
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;
