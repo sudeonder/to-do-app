7cc27f71b8ba1f0d3ec93203fcddc171512918a6
Sude Önder <sudeonder999@gmail.com>
2024-06-08 05:12:57 +0300

    implement signin on be

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,5 +1,6 @@
 import User from "../models/user.js";
 import { createSecretToken } from "../utils/secretToken.js";
+import bcrypt from "bcryptjs";
 
 const signUp = async (req, res, next) => {
   try {
@@ -23,4 +24,32 @@ const signUp = async (req, res, next) => {
   }
 };
 
-export { signUp };
+const signIn = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return res.json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+    const auth = await bcrypt.compare(password, user.password);
+    if (!auth) {
+      return res.json({ message: "Incorrect password or email" });
+    }
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User logged in successfully", success: true });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export { signUp, signIn };
